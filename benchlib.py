9f88da5b1b4502740ce63d7923915da453731dbf
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2009-12-22 20:56:05 -0500

    Refactor some stuff I was getting bored of typing; also add a connection setup/teardown benchmark

---
+++ benchlib.py
@@ -0,0 +1,30 @@
+
+from twisted.internet.defer import Deferred
+
+class Client(object):
+    def __init__(self, reactor):
+        self._reactor = reactor
+        self._requestCount = 0
+
+
+    def run(self, concurrency, duration):
+        self._reactor.callLater(duration, self._stop, None)
+        self._finished = Deferred()
+        for i in range(concurrency):
+            self._request()
+        return self._finished
+
+
+    def _continue(self, ignored):
+        self._requestCount += 1
+        self._request()
+
+
+    def _stop(self, reason):
+        if self._finished is not None:
+            finished = self._finished
+            self._finished = None
+            if reason is not None:
+                finished.errback(reason)
+            else:
+                finished.callback(self._requestCount)
