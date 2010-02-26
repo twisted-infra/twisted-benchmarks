534eea40cfc7ddd633b29265d0a404050081aa77
Jean-Paul Calderone <exarkun@divmod.com>
2010-02-26 14:04:04 -0500

    Try to clean up resources in a timely manner so they have less impact on later iterations

--- names.py
+++ names.py
@@ -9,13 +9,15 @@ from benchlib import Client, driver
 
 
 class Client(Client):
-    def __init__(self, reactor, portNumber):
+    def __init__(self, reactor, portNumber, timeout):
         self._resolver = client.Resolver(servers=[('127.0.0.1', portNumber)])
+        self._timeout = timeout
         super(Client, self).__init__(reactor)
 
 
     def _request(self):
-        d = self._resolver.lookupAddress('localhost')
+        d = self._resolver.lookupAddress(
+            'localhost', timeout=(self._timeout,))
         d.addCallback(self._continue)
         d.addErrback(self._stop)
 
@@ -27,8 +29,15 @@ def main(reactor, duration):
 
     controller = DNSServerFactory([hosts.Resolver()])
     port = reactor.listenUDP(0, DNSDatagramProtocol(controller))
-    client = Client(reactor, port.getHost().port)
+    # Have queries time out no sooner than the duration of this benchmark so
+    # we don't have to deal with retries or timeout errors.
+    client = Client(reactor, port.getHost().port, duration)
     d = client.run(concurrency, duration)
+    def cleanup(passthrough):
+        d = port.stopListening()
+        d.addCallback(lambda ign: passthrough)
+        return d
+    d.addBoth(cleanup)
     return d
 
 
