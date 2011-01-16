e256e8b74b48c8a297985269d58bd8ea55026721
Jean-Paul Calderone <exarkun@divmod.com>
2011-01-16 09:44:12 -0500

    Add an SSH connection setup benchmark.

---
+++ ssh_connect.py
@@ -0,0 +1,79 @@
+
+"""
+Benchmark for SSH connection setup between a Conch client and server using RSA
+keys.
+"""
+
+from twisted.internet.defer import Deferred, succeed
+from twisted.internet.protocol import ClientFactory
+from twisted.conch.ssh.factory import SSHFactory
+from twisted.conch.ssh.keys import Key
+from twisted.conch.ssh.transport import SSHClientTransport
+
+from benchlib import Client, driver
+
+
+PUBLIC_KEY = (
+    'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAGEArzJx8OYOnJmzf4tfBEvLi8DVPrJ3/c9k2I/Az6'
+    '4fxjHf9imyRJbixtQhlH9lfNjUIx+4LmrJH5QNRsFporcHDKOTwTTYLh5KmRpslkYHRivcJSkb'
+    'h/C+BR3utDS555mV')
+
+PRIVATE_KEY = """-----BEGIN RSA PRIVATE KEY-----
+MIIByAIBAAJhAK8ycfDmDpyZs3+LXwRLy4vA1T6yd/3PZNiPwM+uH8Yx3/YpskSW
+4sbUIZR/ZXzY1CMfuC5qyR+UDUbBaaK3Bwyjk8E02C4eSpkabJZGB0Yr3CUpG4fw
+vgUd7rQ0ueeZlQIBIwJgbh+1VZfr7WftK5lu7MHtqE1S1vPWZQYE3+VUn8yJADyb
+Z4fsZaCrzW9lkIqXkE3GIY+ojdhZhkO1gbG0118sIgphwSWKRxK0mvh6ERxKqIt1
+xJEJO74EykXZV4oNJ8sjAjEA3J9r2ZghVhGN6V8DnQrTk24Td0E8hU8AcP0FVP+8
+PQm/g/aXf2QQkQT+omdHVEJrAjEAy0pL0EBH6EVS98evDCBtQw22OZT52qXlAwZ2
+gyTriKFVoqjeEjt3SZKKqXHSApP/AjBLpF99zcJJZRq2abgYlf9lv1chkrWqDHUu
+DZttmYJeEfiFBBavVYIF1dOlZT0G8jMCMBc7sOSZodFnAiryP+Qg9otSBjJ3bQML
+pSTqy7c3a2AScC/YyOwkDaICHnnD3XyjMwIxALRzl0tQEKMXs6hH8ToUdlLROCrP
+EhQ0wahUTCk1gKA4uPD6TMTChavbh4K63OvbKg==
+-----END RSA PRIVATE KEY-----"""
+
+
+class SimpleTransport(SSHClientTransport):
+    def verifyHostKey(self, hostKey, fingerprint):
+        return succeed(True)
+
+
+    def connectionSecure(self):
+        self.transport.loseConnection()
+        self.factory.finished.callback(None)
+
+
+
+class Client(Client):
+    def __init__(self, reactor, port):
+        super(Client, self).__init__(reactor)
+        self._port = port
+
+
+    def _request(self):
+        client = ClientFactory()
+        client.protocol = SimpleTransport
+        client.finished = Deferred()
+        client.finished.addCallback(self._continue)
+        client.finished.addErrback(self._stop)
+        self._reactor.connectTCP('127.0.0.1', self._port, client)
+
+
+
+def main(reactor, duration):
+    server = SSHFactory()
+    server.publicKeys = {
+        'ssh-rsa': Key.fromString(data=PUBLIC_KEY),
+        }
+    server.privateKeys = {
+        'ssh-rsa': Key.fromString(data=PRIVATE_KEY),
+        }
+    port = reactor.listenTCP(0, server)
+    client = Client(reactor, port.getHost().port)
+    d = client.run(1, duration)
+    return d
+
+
+if __name__ == '__main__':
+    import sys
+    import ssh_connect
+    driver(ssh_connect.main, sys.argv)
