b6b287d981d2d02b6460e8e354a3803620a1da3f
Jean-Paul Calderone <exarkun@divmod.com>
2011-01-23 11:48:00 -0500

    Add an SSH throughput benchmark

--- all.py
+++ all.py
@@ -1,11 +1,12 @@
 
 from benchlib import multidriver
 
-import accepts, tcp, iteration, names, threads, web, pb, amp, ssh_connect
+import accepts, tcp, iteration, names, threads, web, pb, amp
+import ssh_connect, ssh_throughput
 
 allBenchmarks = [
     accepts.main, tcp.main, iteration.main, names.main, threads.main,
-    web.main, pb.main, amp.main, ssh_connect.main]
+    web.main, pb.main, amp.main, ssh_connect.main, ssh_throughput.main]
 
 if __name__ == '__main__':
     multidriver(allBenchmarks)
