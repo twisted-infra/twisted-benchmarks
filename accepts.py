8b2f8cc2b224c690bd363db27b7d6b794d479841
Jean-Paul Calderone <exarkun@divmod.com>
2011-01-24 21:28:43 -0500

    Use endpoints in the TCP accepts benchmark, facilitating some code re-use.

--- accepts.py
+++ accepts.py
@@ -1,32 +1,28 @@
 
-from twisted.internet.protocol import ServerFactory, ClientFactory, Protocol
-from twisted.internet.error import ConnectionClosed
-from twisted.internet.defer import Deferred
+from time import time
+
+from twisted.internet.protocol import ServerFactory, Factory, Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint
 
 from benchlib import Client, driver
 
 
 class Client(Client):
-    def __init__(self, reactor, address, portNumber):
+    protocol = Protocol
+
+    def __init__(self, reactor, server):
         super(Client, self).__init__(reactor)
-        self._address = address
-        self._portNumber = portNumber
-        self._factory = ClientFactory()
+        self._server = server
 
 
     def _request(self):
-        finished = Deferred()
-        factory = ClientFactory()
-        factory.protocol = Protocol
-        factory.clientConnectionLost = factory.clientConnectionFailed = lambda connector, reason: finished.errback(reason)
-        finished.addErrback(self._filterFinished)
-        self._reactor.connectTCP(self._address, self._portNumber, factory, bindAddress=(self._address, 0))
-        finished.addCallback(self._continue)
-        finished.addErrback(self._stop)
+        factory = Factory()
+        factory.protocol = self.protocol
 
+        d = self._server.connect(factory)
+        d.addCallback(self._continue)
+        d.addErrback(self._stop)
 
-    def _filterFinished(self, reason):
-        reason.trap(ConnectionClosed)
 
 
 class CloseConnection(Protocol):
@@ -36,17 +32,18 @@ class CloseConnection(Protocol):
 
 interface = 0
 def main(reactor, duration):
-    global interface
     concurrency = 50
 
     factory = ServerFactory()
     factory.protocol = CloseConnection
 
-    interface += 1
-    interface %= 255
-    port = reactor.listenTCP(0, factory, interface='127.0.0.%d' % (interface,))
+    interface = '127.0.0.%d' % (int(time()) % 254 + 1,)
+    port = reactor.listenTCP(0, factory, interface=interface)
 
-    client = Client(reactor, port.getHost().host, port.getHost().port)
+    client = Client(
+        reactor, TCP4ClientEndpoint(
+            reactor, port.getHost().host, port.getHost().port,
+            bindAddress=(interface, 0)))
     d = client.run(concurrency, duration)
     return d
 
