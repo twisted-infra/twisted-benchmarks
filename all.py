cac707e46347ccb58efbc53bae4a9e6af2db0197
Jean-Paul Calderone <exarkun@divmod.com>
2011-02-03 21:32:50 -0500

    Add a memory BIO based version of the SSL benchmarks.

--- all.py
+++ all.py
@@ -2,13 +2,14 @@
 from benchlib import multidriver
 
 import iteration, names, threads, web, pb, amp
-import tcp_connect, ssh_connect, ssl_connect
-import tcp_throughput, ssh_throughput, ssl_throughput
+import tcp_connect, ssh_connect, ssl_connect, sslbio_connect
+import tcp_throughput, ssh_throughput, ssl_throughput, sslbio_throughput
 
 allBenchmarks = [
     iteration.main, names.main, threads.main, web.main, pb.main, amp.main,
-    tcp_connect.main, ssl_connect.main, ssh_connect.main,
-    tcp_throughput.main, ssl_throughput.main, ssh_throughput.main,
+    tcp_connect.main, ssl_connect.main, sslbio_connect.main, ssh_connect.main,
+    tcp_throughput.main, ssl_throughput.main, sslbio_throughput.main,
+    ssh_throughput.main,
     ]
 
 if __name__ == '__main__':
