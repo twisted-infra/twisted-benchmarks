5c1898c79e1f02b02fa77f91a3edc92b336c5139
Jean-Paul Calderone <exarkun@divmod.com>
2011-05-17 13:13:47 -0400

    Allow some benchmarks to be unimportable

--- all.py
+++ all.py
@@ -1,16 +1,24 @@
 
+from twisted.python.reflect import namedAny
+
 from benchlib import multidriver
 
-import iteration, names, threads, web, pb, amp
-import tcp_connect, ssh_connect, ssl_connect, sslbio_connect
-import tcp_throughput, ssh_throughput, ssl_throughput, sslbio_throughput
+allBenchmarkNames = [
+    "iteration", "names", "threads", "web", "pb", "amp",
 
-allBenchmarks = [
-    iteration.main, names.main, threads.main, web.main, pb.main, amp.main,
-    tcp_connect.main, ssl_connect.main, sslbio_connect.main, ssh_connect.main,
-    tcp_throughput.main, ssl_throughput.main, sslbio_throughput.main,
-    ssh_throughput.main,
+    "tcp_connect", "ssh_connect", "ssl_connect", "sslbio_connect",
+    "tcp_throughput", "ssh_throughput", "ssl_throughput", "sslbio_throughput",
     ]
+
+allBenchmarks = []
+
+for name in allBenchmarkNames:
+    try:
+        main = namedAny(name).main
+    except Exception, e:
+        print 'Skipping', name, ':', e
+    else:
+        allBenchmarks.append(main)
 
 if __name__ == '__main__':
     multidriver(allBenchmarks)
